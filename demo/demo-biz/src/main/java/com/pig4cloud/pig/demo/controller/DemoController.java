cb8b1f666b56f19ec3df242d75e49cacf6ef1d87
gsy <3356826110@qq.com>
2024-08-07 17:24:01 +0800

    改为后端滤波，实现列和时间段选择

--- demo/demo-biz/src/main/java/com/pig4cloud/pig/demo/controller/DemoController.java
+++ demo/demo-biz/src/main/java/com/pig4cloud/pig/demo/controller/DemoController.java
@@ -15,6 +15,7 @@ import com.pig4cloud.pig.demo.service.DemoService;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import com.pig4cloud.pig.common.security.annotation.HasPermission;
+import org.apache.ibatis.annotations.Param;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.http.HttpHeaders;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -26,7 +27,12 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 import java.util.*;
+
+//import static com.pig4cloud.pig.demo.mapper.DemoMapper.windowSize;
 
 /**
  * demo 表
@@ -125,19 +131,83 @@ public class DemoController {
     }
 
 	/**
-	 * 查询结果只返回两列
+	 * 数据库的部分时间返回前端
 	 * <p>
-	 * 只显示id、name 两列
+	 * 所有列均显示
 	 */
-	@Operation(summary = "按列查询demo 表" , description = "按列查询demo 表" )
+	@Operation(summary = "数据库的部分时间返回前端" , description = "数据库的部分时间返回前端" )
 	@GetMapping("/select" )
 	@HasPermission("demo_demo_view")
-	public R getSelect() {
-		LambdaQueryWrapper<DemoEntity> wrapper = Wrappers.lambdaQuery();
+	public R getSelect(@RequestParam("startTime") LocalDateTime startTime, @RequestParam("endTime")LocalDateTime endTime) {
+		Integer header= demoMapper.findIdByCreateTime(startTime, "demo");
+		Integer bottom= demoMapper.findIdByCreateTime(endTime, "demo");
 //		wrapper.like("username", "1").lt("id", 40).select("id","name");
 //		wrapper.gt(DemoEntity::getId, 40).select(DemoEntity::getUsername, DemoEntity::getNicename);
-		List<DemoEntity> res = demoService.list(wrapper);
-		return R.ok(res);
+		List<Date> res = demoMapper.selectTime("create_time", "demo");
+		//截取需要的数
+		List<Date> subList = res.subList(header, bottom + 1);
+		return R.ok(subList);
+	}
+
+	/**
+	 * 将滤波后的一列数据返回前端
+	 * <p>
+	 * 所有列均显示
+	 */
+	@Operation(summary = "滤波" , description = "滤波" )
+	@GetMapping("/process" )
+	@HasPermission("demo_demo_view")
+	public R getProcess(@RequestParam("startTime") LocalDateTime startTime, @RequestParam("endTime")LocalDateTime endTime, @RequestParam("columnName")String columnName) {
+//		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//		Date dateTime1 = dateFormat.parse(vary3);
+//		Date dateTime2 = dateFormat.parse(vary4);
+		Integer header= demoMapper.findIdByCreateTime(startTime, "demo");
+		Integer bottom= demoMapper.findIdByCreateTime(endTime, "demo");
+		List<Double> res = demoMapper.selectColumn(columnName, "demo");
+
+		Queue<Double> window = new LinkedList<>();
+		int windowSize = 5; // 滑动窗口的大小
+		// 提取数据列
+		double[] data = new double[res.size()];
+		for (int i = 0; i < res.size(); i++) {
+			data[i] = res.get(i);
+		}
+
+		double[] filteredData = new double[res.size()];
+		
+		// 应用滑动平均滤波
+		for(int i = 0; i < res.size(); i++){
+			filteredData[i] = slidingAverageFilter(data[i], windowSize, window);
+		}
+
+		// 更新平滑后的值到对象
+		for (int i = 0; i < res.size(); i++) {
+			res.set(i, filteredData[i]);
+		}
+
+		//截取需要的数
+		List<Double> subList = res.subList(header, bottom + 1);
+
+		return R.ok(subList);
+	}
+
+	private double slidingAverageFilter(double value, int windowSize, Queue<Double> window) {
+		// 添加当前值到滑动窗口中
+		window.add(value);
+
+		// 如果滑动窗口的大小超过了指定大小，移除最旧的元素
+		if (window.size() > windowSize) {
+			window.poll();
+		}
+
+		// 计算滑动窗口中所有元素的平均值
+		int sum = 0;
+		for (double num : window) {
+			sum += (int) num;
+		}
+		int average = sum / window.size();
+
+		return average;
 	}
 
 	/**
@@ -168,6 +238,19 @@ public class DemoController {
 //	}
 		public R getColumn() {
 		List<String> res = demoMapper.getColumnNames("demo");
+		return R.ok(res);
+	}
+
+	/**
+	 * 查询结果只返回时间
+	 * <p>
+	 * 只显示create_time列
+	 */
+	@Operation(summary = "查询结果只返回时间" , description = "查询结果只返回时间" )
+	@GetMapping("/time" )
+	@HasPermission("demo_demo_view")
+	public R getTime() {
+		List<Date> res = demoMapper.selectTime("create_time", "demo");
 		return R.ok(res);
 	}
 }
--- demo/demo-biz/src/main/java/com/pig4cloud/pig/demo/mapper/DemoMapper.java
+++ demo/demo-biz/src/main/java/com/pig4cloud/pig/demo/mapper/DemoMapper.java
@@ -2,15 +2,29 @@ package com.pig4cloud.pig.demo.mapper;
 
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.pig4cloud.pig.common.core.util.R;
 import com.pig4cloud.pig.demo.entity.DemoEntity;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 @Mapper
 public interface DemoMapper extends BaseMapper<DemoEntity> {
 	@Select("SELECT COLUMN_NAME FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = 'pig_demo' AND TABLE_NAME = #{tableName}")
 	List<String> getColumnNames(@Param("tableName") String tableName);
+
+	@Select("SELECT ID FROM ${tableName} WHERE CREATE_TIME = #{createTime}")
+	Integer findIdByCreateTime(@Param("createTime") LocalDateTime createTime, @Param("tableName") String tableName);
+
+	@Select("SELECT ${columnName} FROM ${tableName}")
+	List<Double> selectColumn(@Param("columnName") String columnName, @Param("tableName") String tableName);
+
+	@Select("SELECT ${columnName} FROM ${tableName}")
+	List<Date> selectTime(@Param("columnName") String columnName, @Param("tableName") String tableName);
 }
